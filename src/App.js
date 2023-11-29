60e348b47ccd48b96adfd622d9341ee1e2b7728b
Grasiele Pacheco <grasiele2191@gmail.com>
2023-11-29 11:14:56 +0100

    solution for 1st task. problem with vs code, need to finish bonus

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍓 Strawberry",
+      color: "red",
+    },
+    {
+      id: 1339,
+      name: "🥭 Mango",
+      color: "yellow",
+    },
+    {
+      id: 1340,
+      name: "🥑 Abacate",
+      color: "green",
+    },
+  ];
 
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      <ul>
+        {fruits.map((fruit) => (
+          <Card id={fruit.id} name={fruit.name} color={fruit.color} />
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,9 @@
 import "./Card.css";
 
-export default function Card({ name }) {
-  return <p className="card">{name}</p>;
+export default function Card({ id, name, color }) {
+  return (
+    <p className="card">
+      {id} {name} {color}
+    </p>
+  );
 }
